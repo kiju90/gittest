b065e1dd77f926fb25e3d5b6f6966b90371223be
LG <LG@LG-PC>
2017-01-03 10:59:55 +0900

    수정3

--- src/basic/Test3.java
+++ src/basic/Test3.java
@@ -5,7 +5,7 @@ public class Test3 {
 	public static void main(String[] args) {
 		System.out.println("git test....");
 		System.out.println("git test....");
-		System.out.println("git test....");
+		System.out.println("¼öÁ¤");
 	}
 
 }
